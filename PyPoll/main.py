1f0b4efff9981348df09c77d7e393c3923a47611
Takako Omori <francisca661@gmail.com>
2018-03-12 17:03:15 -0700

    edited Poll

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -7,9 +7,19 @@ csv_data1 = os.path.join('raw_data','election_data_1.csv')
 with open(csv_data1,'r') as csvfile:
     csvreader1 = csv.reader(csvfile,delimiter=',')
     for row in csvreader1:
-        if row[0] == int:
-            
+        totalVotes = sum(1 for row in csvfile) - 1
 
+        if row[2] = Roger:
+            totalRoger = count
+
+        if row[2] = Gomez:
+            totalGomez = count
+
+        if row[2] = Brentwood:
+            totalBrentwood = count
+
+        if row[2] = Higgins:
+            totalHiggins = count
 
 
 
@@ -17,10 +27,10 @@ print("Election Results")
 print("-----------------")
 print("Total Votes: " + totalVotes)
 print("-----------------")
-print("Rogers: "+ )
-print("Gomez: " + )
-print("Brentwood: " + )
-print("Higgins: "+ )
+print("Rogers: "+ totalRoger)
+print("Gomez: " + totalGomez)
+print("Brentwood: " + totalBrentwood)
+print("Higgins: "+ totalHiggins)
 print("------------------")
-print("Winner: " + )
+print("Winner: " + winner)
 print("------------------")
